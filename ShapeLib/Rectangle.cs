84d86ea96e4ce57b3e955462aba369186c48cb82
Elie <Elie@CODEVALUE>
2016-07-03 19:09:25 +0300

    Add project files.

---
+++ ShapeLib/Rectangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeLib
+{
+    public class Rectangle : Shape
+    {
+        public override double Area { get; }
+
+        public int _width;
+        public int _height;
+
+        public Rectangle(int width, int height)
+        {
+
+        }
+
+
+    }
+}
---
+++ ShapeLib/Shape.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeLib
+{
+    public abstract class Shape
+    {
+        public ConsoleColor Color { get; set; }
+
+        public abstract double Area { get; }
+
+        public Shape(ConsoleColor color)
+        {
+            Color = color;
+        }
+
+        public Shape()
+        {
+            Color = ConsoleColor.White;
+        }
+
+        public virtual void Display()
+        {
+            Console.BackgroundColor = Color;
+        }
+
+    }
+}
